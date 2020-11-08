decaf148a8e3bbf47bfaba8a3c396349af5ff90e
Manuel Carvalho <burrowsrd@gmail.com>
2020-11-08 21:18:58 +1100

    Added functions 3

--- banner.py
+++ banner.py
@@ -1,4 +1,4 @@
-def banner_text(text, screen_width=80):
+def banner_text(text=" ", screen_width=80):
     # screen_width = 50
     if len(text) > screen_width - 4:
         # print("EEK!!")
@@ -19,7 +19,7 @@ banner_text("Always look on the bright side of life...")
 banner_text("If life seems jolly rotten,")
 banner_text("There's something you've forgotten!")
 banner_text("And that's to laugh and smile and dance and sing,")
-banner_text(" ")
+banner_text(screen_width=60)
 banner_text("When you're feeling in the dumps,")
 banner_text("Don't be silly chumps,")
 banner_text("Just purse your lips and whistle - that's the thing!")
--- guessing_game.py
+++ guessing_game.py
@@ -1,13 +1,27 @@
 def get_integer(prompt):
+    """
+    Get an integer from Standard Input (stdin).
+
+    The function will continue looping, and prompting
+    the user, until a valid 'int' is entered.
+
+    :param prompt: The string that the user will see, when
+        they're prompted to enter the value
+    :return: The integer that the user enters.
+    """
     while True:
         temp = input(prompt)
         if temp.isnumeric():
             return int(temp)
 
+print(input.__doc__)
+print("*" * 80)
+print(get_integer.__doc__)
+print("*" * 80)
 answer = 5
 
 print("Please pick a number between 1 and 10: ")
-guess = get_integer(": ")
+guess = get_integer(": ") #ctrl j to get doc strings
 
 if guess != answer:
     if guess < answer:
